b3089be124c0f2bcb802759a8ad61f9e2ef665ff
bharathsekar7777@gmail.com <bharathsekar7777@gmail.com>
2025-09-20 10:33:51 +0530

    1.5

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const dotenv = require('dotenv')
-import { Configuration, OpenAIApi } from "openai";
+const { Configuration, OpenAIApi } = require("openai");
 
 const app = express()
 
